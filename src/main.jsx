7ebcd1c557f732bcb86ebed9e60af6923af5bbb4
Shivam Bansal <shivambansal@Shivams-MacBook-Air.local>
2025-08-05 19:51:40 +0530

    changes on vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Todo from './Todo'
+import TodoApp from './TodoApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Todo />
+  <TodoApp />
   </StrictMode>
 )
